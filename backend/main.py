edd58249466ce27f54bab31e96474fcb9066bcf9
r-y-g-u-y <ryan.bate@hotmail.com>
2021-03-14 10:17:12 -0500

    Refactored for good

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ PORT = 9090
 class theHttpHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = '../www/index.html'
+            self.path = '../www/evil.html'
         return super().do_GET()
 
 handler = theHttpHandler
