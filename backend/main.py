856185892aac69b096c16341ca9d1404ecd75afe
r-y-g-u-y <ryan.bate@hotmail.com>
2021-03-14 00:00:14 -0600

    Added webserver functionality

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,26 @@ import http.server
 import socketserver
 
 PORT = 9090
-handler = http.server.SimpleHTTPRequestHandler(directory="../www")
+
+
+class theHttpHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '../www/index.html'
+        return super().do_GET()
+
+handler = theHttpHandler
+
+my_server = socketserver.TCPServer(("", PORT), handler)
+
+my_server.serve_forever()
+
+
+
+
+
+
+
 
 with socketserver.TCPServer(("", PORT), handler) as httpd:
     httpd.serve_forever()
